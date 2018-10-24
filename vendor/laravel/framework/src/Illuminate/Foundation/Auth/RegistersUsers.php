aedebb3f89dff9e37c9ef7f97c17e4efefffe4fe
mggj <wujiankun1998@qq.com>
2018-10-24 20:04:35 +0800

    modify readme.md

--- vendor/laravel/framework/src/Illuminate/Foundation/Auth/RegistersUsers.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Auth/RegistersUsers.php
@@ -26,23 +26,23 @@ trait RegistersUsers
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function register(Request $request)
-    {
-        $this->validator($request->all())->validate();
+    // public function register(Request $request)
+    // {
+    //     $this->validator($request->all())->validate();
 
-        event(new Registered($user = $this->create($request->all())));
+    //     event(new Registered($user = $this->create($request->all())));
 
-        $this->sendEmailConfirmationTo($user);
-        session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
-        return $this->registered($request, $user)
-                        ?: redirect($this->redirectPath());
+    //     $this->sendEmailConfirmationTo($user);
+    //     session()->flash('success', '验证邮件已发送到你的注册邮箱上，请注意查收。');
+    //     return $this->registered($request, $user)
+    //                     ?: redirect($this->redirectPath());
         
-        // return redirect('/');
-        // $this->guard()->login($user);
+    //     // return redirect('/');
+    //     // $this->guard()->login($user);
 
-        // session()->flash('success', $user->name.'欢迎来到 Cubing 师大，您将在这里开启一段新的旅程~');
+    //     // session()->flash('success', $user->name.'欢迎来到 Cubing 师大，您将在这里开启一段新的旅程~');
 
-    }
+    // }
 
     /**
      * Get the guard to be used during registration.
@@ -66,17 +66,17 @@ trait RegistersUsers
         //
     }
 
-    public function sendEmailConfirmationTo($user)
-    {
-        $view = 'auth.emails.confirm';
-        $data = compact('user');
-        $from = 'aufree@yousails.com';
-        $name = 'Aufree';
-        $to = $user->email;
-        $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
+    // public function sendEmailConfirmationTo($user)
+    // {
+    //     $view = 'auth.emails.confirm';
+    //     $data = compact('user');
+    //     $from = 'aufree@yousails.com';
+    //     $name = 'Aufree';
+    //     $to = $user->email;
+    //     $subject = "感谢注册 Sample 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
-        });
-    }
+    //     Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
+    //         $message->from($from, $name)->to($to)->subject($subject);
+    //     });
+    // }
 }
