057b47d536dcef67818b95011a2c954b643ddf59
MGGJ <wujiankun1998@qq.com>
2018-11-07 15:50:20 +0800

    temp commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,6 +30,56 @@
             border-top: 4px solid #a5d6fb;
             margin-top: 0px;
         }
+
+        /* 自定义三级菜单 */
+        .dropdown-submenu {
+            position: relative;
+        }
+
+        .dropdown-submenu>.dropdown-menu {
+            top: 0;
+            left: 100%;
+            margin-top: -6px;
+            margin-left: -1px;
+            -webkit-border-radius: 0 6px 6px 6px;
+            -moz-border-radius: 0 6px 6px;
+            border-radius: 0 6px 6px 6px;
+        }
+
+        .dropdown-submenu:hover>.dropdown-menu {
+            display: block;
+        }
+
+        .dropdown-submenu>a:after {
+            display: block;
+            content: " ";
+            float: right;
+            width: 0;
+            height: 0;
+            border-color: transparent;
+            border-style: solid;
+            border-width: 5px 0 5px 5px;
+            border-left-color: #ccc;
+            margin-top: 5px;
+            margin-right: -10px;
+        }
+
+        .dropdown-submenu:hover>a:after {
+            border-left-color: #fff;
+        }
+
+        .dropdown-submenu.pull-left {
+            float: none;
+        }
+
+        .dropdown-submenu.pull-left>.dropdown-menu {
+            left: -100%;
+            margin-left: 10px;
+            -webkit-border-radius: 6px 0 6px 6px;
+            -moz-border-radius: 6px 0 6px 6px;
+            border-radius: 6px 0 6px 6px;
+        }
+        /* 三级下拉菜单 */
         @yield('style')
     </style>
 
@@ -80,29 +130,35 @@
                                     抽奖
                                 </a>
                             </li>
-                            <li class="divider"></li>
+                            <li class="divider col-lg-10 col-lg-offset-1 col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1"></li>
                             <li>
                                 <a href="http://algdb.net/">
-                                    <i class="fas fa-link"></i>
+                                    <i class="far fa-hand-point-right"></i>
                                     AlgDB 公式库
                                 </a>
                             </li>
                             <li>
                                 <a href="https://alg.cubing.net/">
-                                    <i class="fas fa-link"></i>
+                                    <i class="far fa-hand-point-right"></i>
                                     魔方图案生成器
                                 </a>
                             </li>
                             <li>
                                 <a href="https://huazhechen.gitee.io/cuber/">
-                                    <i class="fas fa-link"></i>
-                                    魔方动态图生成器
+                                    <i class="far fa-hand-point-right"></i>
+                                    3D 魔方
                                 </a>
                             </li>
                             <li>
                                 <a href="https://www.cubeskills.com/tools/pll-recognition-trainer/basic-test">
-                                    <i class="fas fa-link"></i>
+                                    <i class="far fa-hand-point-right"></i>
                                     PLL 训练（6 格观察法）
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://fewestmov.es/">
+                                    <i class="far fa-hand-point-right"></i>
+                                    最少步插入查找工具
                                 </a>
                             </li>
                         </ul>
